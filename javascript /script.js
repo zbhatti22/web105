e331cff3751443883554719520f7917ea00fff21
zbhatti22 <zbhatti@students.mchenry.edu>
2023-10-26 11:49:27 -0500

    comitted all files to github

---
+++ javascript /script.js
@@ -0,0 +1,9 @@
+/* https://www.w3schools.com/js/tryit.asp?filename=tryjs_intro_lightbulb */
+
+
+
+/* change the image */
+function change(picture) {
+    document.getElementById("large").src = picture;   
+
+}
